e8cdcbb8b315fb890543c054575ebf61529e392c
이예찬 <blackmagic2@naver.com>
2019-05-05 01:43:48 +0900

    불필요한 주석 제거, Game.GetPlayerName 메소드 HasValue 쪽 수정

--- IPlayer.py
+++ IPlayer.py
@@ -3,9 +3,12 @@ from abc import *
 class IPlayer(metaclass=ABCMeta):
     name = ""
     identifier = ""
+
     def __init__(self, name, identifier):
         self.Name = name
         self.Identifier = identifier
+
+    @abstractmethod
     def NextMove(self, currentBoard):
         pass
 
