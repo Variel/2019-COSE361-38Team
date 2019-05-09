473853d76184b7e7a0310641f57212f80e203187
Jiyeon Jeon <mmpi2004@naver.com>
2019-05-09 21:31:20 +0900

    약간의 수정.

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Game import Game
 from AiPlayer import AiPlayer
 from HumanPlayer import HumanPlayer
 
-Game(AiPlayer('samsung', False), HumanPlayer('apple', True)).start_loop()
+Game(AiPlayer('samsung', False), HumanPlayer('사람', True)).start_loop()
